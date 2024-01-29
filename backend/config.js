77b12e3f815b896d8f956e784c45df1d838db4c3
Alam <69905764+alamansari11@users.noreply.github.com>
2024-01-29 12:30:33 +0530

    Update config.js  masked uri

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
-export const MONGODB_URL = 'mongodb+srv://alamdev33:alamdev33@cluster0.esnjkzz.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const MONGODB_URL = 'masked'
 
 // originally the url was like "mongodb+srv://alamdev33:<password>@cluster0.esnjkzz.mongodb.net/?retryWrites=true&w=majority"
